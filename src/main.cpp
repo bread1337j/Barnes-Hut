485c0289839118f19d23184eb05a9f1fdd6754dd
bread1337j <131266168+bread1337j@users.noreply.github.com>
2026-09-11 15:45:26 -0700

    perhaps there is no purpose to running the simulation on every tick while it is paused...

--- src/main.cpp
+++ src/main.cpp
@@ -13,12 +13,12 @@ void traverseQuadNode(quadTree* tree, int node, float zoom){
 	Color c = GREEN;
 	DrawRectangleLinesEx({ (float)tree->access(node).x, (float)tree->access(node).y, (float)tree->access(node).size_x, (float)tree->access(node).size_x }, 1.0f / zoom, c);
 	/*if(tree->access(node).count > 0){
-		DrawLineV({(float)tree->access(node).sx/tree->access(node).count, (float)tree->access(node).sy/tree->access(node).count}, { (float)tree->access(node).x, (float)tree->access(node).y }, YELLOW);
-		DrawLineV({(float)tree->access(node).sx/tree->access(node).count, (float)tree->access(node).sy/tree->access(node).count}, { (float)(tree->access(node).x+tree->access(node).size_x), (float)(tree->access(node).y) }, YELLOW);
-		DrawLineV({(float)tree->access(node).sx/tree->access(node).count, (float)tree->access(node).sy/tree->access(node).count}, { (float)(tree->access(node).x+tree->access(node).size_x), (float)(tree->access(node).y+tree->access(node).size_y) }, YELLOW);
-		DrawLineV({(float)tree->access(node).sx/tree->access(node).count, (float)tree->access(node).sy/tree->access(node).count}, { (float)(tree->access(node).x), (float)(tree->access(node).y+tree->access(node).size_y) }, YELLOW);
-		DrawCircleV({ (float)tree->access(node).sx/tree->access(node).count, (float) tree->access(node).sy/tree->access(node).count }, (float) tree->access(node).size_x/100, RED);
-	}*/
+	  DrawLineV({(float)tree->access(node).sx/tree->access(node).count, (float)tree->access(node).sy/tree->access(node).count}, { (float)tree->access(node).x, (float)tree->access(node).y }, YELLOW);
+	  DrawLineV({(float)tree->access(node).sx/tree->access(node).count, (float)tree->access(node).sy/tree->access(node).count}, { (float)(tree->access(node).x+tree->access(node).size_x), (float)(tree->access(node).y) }, YELLOW);
+	  DrawLineV({(float)tree->access(node).sx/tree->access(node).count, (float)tree->access(node).sy/tree->access(node).count}, { (float)(tree->access(node).x+tree->access(node).size_x), (float)(tree->access(node).y+tree->access(node).size_y) }, YELLOW);
+	  DrawLineV({(float)tree->access(node).sx/tree->access(node).count, (float)tree->access(node).sy/tree->access(node).count}, { (float)(tree->access(node).x), (float)(tree->access(node).y+tree->access(node).size_y) }, YELLOW);
+	  DrawCircleV({ (float)tree->access(node).sx/tree->access(node).count, (float) tree->access(node).sy/tree->access(node).count }, (float) tree->access(node).size_x/100, RED);
+	  }*/
 	if(tree->access(node).children == -1){
 		return;
 	}
@@ -33,15 +33,16 @@ int main(){
 	quadTree* qtree = new quadTree(0, 0);
 	double leftCorner = 0;
 	double rightCorner = 0;
+	int debounce = 1;
 	SetConfigFlags(FLAG_VSYNC_HINT);
 	SetConfigFlags(FLAG_WINDOW_RESIZABLE);
 	InitWindow(800, 600, "quad tree test");
 	//SetTargetFPS(60);
 	/*qtree->root->divide();
-	for(int i=0; i<4; i++){
-		(qtree->root->children+i)->divide();
-	}*/
-	
+	  for(int i=0; i<4; i++){
+	  (qtree->root->children+i)->divide();
+	  }*/
+
 	std::random_device rd;
 	std::mt19937 e2(rd());
 	std::uniform_real_distribution<> dist(0.0f, 1.0f);
@@ -57,12 +58,12 @@ int main(){
 		objarr[i].vx = (dist(e2)-0.5)*0.1f;
 		objarr[i].vy = (dist(e2)-0.5)*0.1f;
 	}
-	
+
 	char* title = (char*) (malloc(sizeof(char) * 100));
 	double t = 0;
 	int iters = 0;
 	double dtDef = 0.0001;
-	
+
 	double dt = dtDef;
 
 	Camera2D camera = { 0 };
@@ -70,10 +71,12 @@ int main(){
 	int drawTree = 0;
 	int infinitisemalParticles = 1;
 	int render = 1;
+	int running = 1;
 
 	double spawnedMass = 8e13;
+
 	while(!WindowShouldClose()){
-		
+
 		//qtree->root->divide(500, 400);
 		if(IsMouseButtonDown(MOUSE_BUTTON_RIGHT)){
 			Vector2 pos = GetMousePosition();
@@ -111,11 +114,7 @@ int main(){
 			camera.target = Vector2Add(camera.target, delta);
 		}
 		if(IsKeyPressed(KEY_SPACE)){
-			if(dt == 0){
-				dt = dtDef;
-			}else{
-				dt = 0;
-			}
+			running = !running;
 		}
 		if(IsKeyPressed(KEY_LEFT)){
 			dt *= 0.9;
@@ -135,81 +134,83 @@ int main(){
 		if(IsKeyPressed(KEY_R)){
 			render = !render;
 		}
+		if(running){
+			leftCorner = rightCorner = objarr[0].x;
+			for(int i=0; i<objarr.size(); i++){
+				objarr[i].kick(dt);
+			}
+			//printf("Trying to clear the qtree\n");
+			//printf("Cleared the qtree, new size should be 1, is: %lu\n", qtree->tree.size());
 
-		leftCorner = rightCorner = objarr[0].x;
-		for(int i=0; i<objarr.size(); i++){
-			objarr[i].kick(dt);
-		}
-		//printf("Trying to clear the qtree\n");
-		//printf("Cleared the qtree, new size should be 1, is: %lu\n", qtree->tree.size());
-		
-		for(int i=0; i<objarr.size(); i++){
-			objarr[i].refine(*qtree);
-		}
-		//printf("Propogating the new qtree\n");
-		for(int i=0; i<objarr.size(); i++){
-			objarr[i].propogate(*qtree);
-		}
-		qtree->propogate();
-		
-	
+			for(int i=0; i<objarr.size(); i++){
+				objarr[i].refine(*qtree);
+			}
+			//printf("Propogating the new qtree\n");
+			for(int i=0; i<objarr.size(); i++){
+				objarr[i].propogate(*qtree);
+			}
+			qtree->propogate();
 
-		//printf("Doing gravity\n");
-		for(int i=0; i<objarr.size(); i++){
-			objarr[i].drift(*qtree, dt); 
-		}
-		for(int i=0; i<objarr.size(); i++){
-			objarr[i].move(dt);
-			if(objarr[i].x > rightCorner){
-				rightCorner = objarr[i].x;
+
+
+			//printf("Doing gravity\n");
+			for(int i=0; i<objarr.size(); i++){
+				objarr[i].drift(*qtree, dt); 
 			}
-			if(objarr[i].y > rightCorner){
-				rightCorner = objarr[i].y;
+			for(int i=0; i<objarr.size(); i++){
+				objarr[i].move(dt);
+				if(objarr[i].x > rightCorner){
+					rightCorner = objarr[i].x;
+				}
+				if(objarr[i].y > rightCorner){
+					rightCorner = objarr[i].y;
+				}
+				if(objarr[i].x < leftCorner){
+					leftCorner = objarr[i].x;
+				}
+				if(objarr[i].y < leftCorner){
+					leftCorner = objarr[i].y;
+				}
+				objarr[i].calcKey();
 			}
-			if(objarr[i].x < leftCorner){
-				leftCorner = objarr[i].x;
+			qtree->tree.reserve(1 + objarr.size() * 4);
+			qtree->clear(leftCorner, rightCorner-leftCorner);
+			for(int i=0; i<objarr.size(); i++){
+				objarr[i].kick(dt); 
 			}
-			if(objarr[i].y < leftCorner){
-				leftCorner = objarr[i].y;
+			std::sort(objarr.begin(), objarr.end());
+			for(int i=0; i<objarr.size(); i++){
+
+				objarr[i].grabNode(*qtree);
 			}
-			objarr[i].calcKey();
-		}
-		qtree->tree.reserve(1 + objarr.size() * 4);
-		qtree->clear(leftCorner, rightCorner-leftCorner);
-		for(int i=0; i<objarr.size(); i++){
-			objarr[i].kick(dt); 
-		}
-		std::sort(objarr.begin(), objarr.end());
-		for(int i=0; i<objarr.size(); i++){
-			
-			objarr[i].grabNode(*qtree);
 		}
 
 		//printf("Drawing\n");
 		BeginDrawing();
-			BeginMode2D(camera);
-				if(render){
-					ClearBackground(BLACK);
-					for(int i=0; i<objarr.size(); i++){
-						if(infinitisemalParticles){
-							DrawCircleV({(float)objarr[i].x, (float)objarr[i].y}, 8 / camera.zoom, DARKBLUE);
-						}else{
-							DrawPixelV({(float) objarr[i].x, (float)objarr[i].y}, DARKBLUE);
-							//DrawCircleV({(float)objarr[i].x, (float)objarr[i].y}, 8, DARKBLUE);
-						}
-					}
-					if(drawTree){
-						traverseQuadNode(qtree, qtree->root, camera.zoom);
-					}
+		BeginMode2D(camera);
+		if(render){
+			ClearBackground(BLACK);
+			for(int i=0; i<objarr.size(); i++){
+				if(infinitisemalParticles){
+					DrawCircleV({(float)objarr[i].x, (float)objarr[i].y}, 8 / camera.zoom, DARKBLUE);
+				}else{
+					DrawPixelV({(float) objarr[i].x, (float)objarr[i].y}, DARKBLUE);
+					//DrawCircleV({(float)objarr[i].x, (float)objarr[i].y}, 8, DARKBLUE);
 				}
+			}
+			if(drawTree){
+				traverseQuadNode(qtree, qtree->root, camera.zoom);
+			}
+		}
 
-			EndMode2D();
+		EndMode2D();
 		EndDrawing();
 		sprintf(title, "%f | %lu", 1/GetFrameTime(), objarr.size());
 		SetWindowTitle(title);
 		t+=dt;
 		iters++;
 	}
+	debounce = 0;
 
 	CloseWindow();
 
